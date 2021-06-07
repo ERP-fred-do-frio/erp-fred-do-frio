4bc945488031a614db9cd86cf714d0731412f498
Genesisferreira <genesisferreira2010@hotmail.com>
2021-06-07 19:17:29 -0300

    tela1

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$('.Cadastro').click(function(){
+    $('.menuLateral ul .itensCadastro').toggleClass('mostra');
+});
+$('.Estoque').click(function(){
+    $('.menuLateral ul .itensEstoque').toggleClass('mostra');
+});
+$('.Vendas').click(function(){
+    $('.menuLateral ul .itensVendas').toggleClass('mostra');
+});
+$('.Financeiro').click(function(){
+    $('.menuLateral ul .itensFinanceiros').toggleClass('mostra');
+});
+$('.Relatorios').click(function(){
+    $('.menuLateral ul .itensRelatorios').toggleClass('mostra');
+});
+$('.Notas').click(function(){
+    $('.menuLateral ul .itensNotas').toggleClass('mostra');
+});
+$('.Lojas').click(function(){
+    $('.menuLateral ul .itensLojas').toggleClass('mostra');
+});
+$('.Contratos').click(function(){
+    $('.menuLateral ul .itensContratos').toggleClass('mostra');
+});
+$('.Atendimentos').click(function(){
+    $('.menuLateral ul .itensAtendimento').toggleClass('mostra');
+});
+$('.Agenda').click(function(){
+    $('.menuLateral ul .itensAgenda').toggleClass('mostra');
+});
+$('.Funcionarios').click(function(){
+    $('.menuLateral ul .itensFuncionarios').toggleClass('mostra');
+});
+
+$('.btnAbre').click(function(){
+    $('.menuLateral').toggleClass('mostra');
+});
+$('.btnFecha').click(function(){
+    $('.menuLateral').toggleClass('mostra');
+});
